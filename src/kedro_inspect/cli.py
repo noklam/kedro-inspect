9ccb983f45e7ce708e6c2344163baf9a9332563d
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-02-10 22:22:43 +0000

    add kedro-inspect compile

--- src/kedro_inspect/cli.py
+++ src/kedro_inspect/cli.py
@@ -2,15 +2,23 @@ from pathlib import Path
 import typer
 from kedro_inspect.core import KedroProject
 
-
-def main():
+app = typer.Typer()
+# compile_app = typer.Typer()
+# app.add_typer(compile_app, name="compile", help="Compile configurations.")
+@app.command()
+def datasets():
     project_path = "demo-project"
     project_path = Path(project_path).resolve()
     project = KedroProject(project_path)
     unused_datasets = project.find_unused_datasets()
     typer.echo(unused_datasets)
 
-
+@app.command()
+def compile():
+    print("Compile")
+    ...
 # Direct Usage
 # Implicit Usage (dictionary)
 #
+if __name__ == "__main__":
+    app()
